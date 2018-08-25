39e22804bcd31311365aa9910473d01efa281cde
marisn <marisn@67cc.cn>
2018-08-25 13:23:12 +0800

    update
    update

--- sspanel.sql
+++ sspanel.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.15.10
--- https://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 2018-08-25 11:51:48
--- 服务器版本： 5.6.37-log
--- PHP Version: 7.2.3
+-- Generation Time: 2018-08-25 13:21:51
+-- 服务器版本： 5.5.60-log
+-- PHP Version: 7.1.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `1`
+-- Database: `sspanel`
 --
 
 -- --------------------------------------------------------
@@ -26,7 +28,7 @@ SET time_zone = "+00:00";
 -- 表的结构 `alive_ip`
 --
 
-CREATE TABLE IF NOT EXISTS `alive_ip` (
+CREATE TABLE `alive_ip` (
   `id` bigint(20) NOT NULL,
   `nodeid` int(11) NOT NULL,
   `userid` int(11) NOT NULL,
@@ -40,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `alive_ip` (
 -- 表的结构 `announcement`
 --
 
-CREATE TABLE IF NOT EXISTS `announcement` (
+CREATE TABLE `announcement` (
   `id` int(11) NOT NULL,
   `date` datetime NOT NULL,
   `content` longtext NOT NULL,
@@ -53,13 +55,13 @@ CREATE TABLE IF NOT EXISTS `announcement` (
 -- 表的结构 `auto`
 --
 
-CREATE TABLE IF NOT EXISTS `auto` (
+CREATE TABLE `auto` (
   `id` bigint(20) NOT NULL,
   `type` int(11) NOT NULL,
   `value` longtext NOT NULL,
   `sign` longtext NOT NULL,
   `datetime` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -67,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `auto` (
 -- 表的结构 `blockip`
 --
 
-CREATE TABLE IF NOT EXISTS `blockip` (
+CREATE TABLE `blockip` (
   `id` bigint(20) NOT NULL,
   `nodeid` int(11) NOT NULL,
   `ip` text NOT NULL,
@@ -80,7 +82,7 @@ CREATE TABLE IF NOT EXISTS `blockip` (
 -- 表的结构 `bought`
 --
 
-CREATE TABLE IF NOT EXISTS `bought` (
+CREATE TABLE `bought` (
   `id` bigint(20) NOT NULL,
   `userid` bigint(20) NOT NULL,
   `shopid` bigint(20) NOT NULL,
@@ -88,7 +90,7 @@ CREATE TABLE IF NOT EXISTS `bought` (
   `renew` bigint(11) NOT NULL,
   `coupon` text NOT NULL,
   `price` decimal(12,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -96,7 +98,7 @@ CREATE TABLE IF NOT EXISTS `bought` (
 -- 表的结构 `code`
 --
 
-CREATE TABLE IF NOT EXISTS `code` (
+CREATE TABLE `code` (
   `id` bigint(20) NOT NULL,
   `code` text NOT NULL,
   `type` int(11) NOT NULL,
@@ -112,14 +114,14 @@ CREATE TABLE IF NOT EXISTS `code` (
 -- 表的结构 `coupon`
 --
 
-CREATE TABLE IF NOT EXISTS `coupon` (
+CREATE TABLE `coupon` (
   `id` bigint(20) NOT NULL,
   `code` text NOT NULL,
   `onetime` int(11) NOT NULL,
   `expire` bigint(20) NOT NULL,
   `shop` text NOT NULL,
   `credit` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -127,13 +129,13 @@ CREATE TABLE IF NOT EXISTS `coupon` (
 -- 表的结构 `detect_list`
 --
 
-CREATE TABLE IF NOT EXISTS `detect_list` (
+CREATE TABLE `detect_list` (
   `id` bigint(20) NOT NULL,
   `name` longtext NOT NULL,
   `text` longtext NOT NULL,
   `regex` longtext NOT NULL,
   `type` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -141,13 +143,13 @@ CREATE TABLE IF NOT EXISTS `detect_list` (
 -- 表的结构 `detect_log`
 --
 
-CREATE TABLE IF NOT EXISTS `detect_log` (
+CREATE TABLE `detect_log` (
   `id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `list_id` bigint(20) NOT NULL,
   `datetime` bigint(20) NOT NULL,
   `node_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -155,12 +157,12 @@ CREATE TABLE IF NOT EXISTS `detect_log` (
 -- 表的结构 `disconnect_ip`
 --
 
-CREATE TABLE IF NOT EXISTS `disconnect_ip` (
+CREATE TABLE `disconnect_ip` (
   `id` bigint(20) NOT NULL,
   `userid` bigint(20) NOT NULL,
   `ip` text NOT NULL,
   `datetime` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -168,13 +170,13 @@ CREATE TABLE IF NOT EXISTS `disconnect_ip` (
 -- 表的结构 `email_verify`
 --
 
-CREATE TABLE IF NOT EXISTS `email_verify` (
+CREATE TABLE `email_verify` (
   `id` bigint(20) NOT NULL,
   `email` text NOT NULL,
   `ip` text NOT NULL,
   `code` text NOT NULL,
   `expire_in` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -182,7 +184,7 @@ CREATE TABLE IF NOT EXISTS `email_verify` (
 -- 表的结构 `link`
 --
 
-CREATE TABLE IF NOT EXISTS `link` (
+CREATE TABLE `link` (
   `id` bigint(20) NOT NULL,
   `type` int(11) NOT NULL,
   `address` text NOT NULL,
@@ -195,13 +197,24 @@ CREATE TABLE IF NOT EXISTS `link` (
   `method` text
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `link`
+--
+
+INSERT INTO `link` (`id`, `type`, `address`, `port`, `token`, `ios`, `userid`, `isp`, `geo`, `method`) VALUES
+(1, -1, 'smart', 0, 'e7gkJ71dJ0jJ5ujd', 1, 1, NULL, 0, 'smart'),
+(2, 9, 'smart', 0, 'Wfs6XRy1m2k3y5MC', 0, 1, NULL, 0, 'smart'),
+(3, 10, '', 0, 'AmnT7EEM7dyVfQHC', 0, 1, NULL, 0, ''),
+(4, 10, '', 0, 'zje3QIM0VqzAVUip', 0, 1, NULL, 1, ''),
+(5, 11, '', 0, 'iXRkaWqPN2hPcAhY', 0, 1, NULL, 0, '');
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `login_ip`
 --
 
-CREATE TABLE IF NOT EXISTS `login_ip` (
+CREATE TABLE `login_ip` (
   `id` bigint(20) NOT NULL,
   `userid` bigint(20) NOT NULL,
   `ip` text NOT NULL,
@@ -209,20 +222,27 @@ CREATE TABLE IF NOT EXISTS `login_ip` (
   `type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- 转存表中的数据 `login_ip`
+--
+
+INSERT INTO `login_ip` (`id`, `userid`, `ip`, `datetime`, `type`) VALUES
+(1, 1, '112.193.42.16', 1535174490, 0);
+
 -- --------------------------------------------------------
 
 --
 -- 表的结构 `payback`
 --
 
-CREATE TABLE IF NOT EXISTS `payback` (
+CREATE TABLE `payback` (
   `id` bigint(20) NOT NULL,
   `total` decimal(12,2) NOT NULL,
   `userid` bigint(20) NOT NULL,
   `ref_by` bigint(20) NOT NULL,
   `ref_get` decimal(12,2) NOT NULL,
   `datetime` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -230,7 +250,7 @@ CREATE TABLE IF NOT EXISTS `payback` (
 -- 表的结构 `paylist`
 --
 
-CREATE TABLE IF NOT EXISTS `paylist` (
+CREATE TABLE `paylist` (
   `id` bigint(20) NOT NULL,
   `userid` bigint(20) NOT NULL,
   `total` decimal(12,2) NOT NULL,
@@ -245,7 +265,7 @@ CREATE TABLE IF NOT EXISTS `paylist` (
 -- 表的结构 `radius_ban`
 --
 
-CREATE TABLE IF NOT EXISTS `radius_ban` (
+CREATE TABLE `radius_ban` (
   `id` int(11) NOT NULL,
   `userid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -256,7 +276,7 @@ CREATE TABLE IF NOT EXISTS `radius_ban` (
 -- 表的结构 `relay`
 --
 
-CREATE TABLE IF NOT EXISTS `relay` (
+CREATE TABLE `relay` (
   `id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `source_node_id` bigint(20) NOT NULL,
@@ -272,7 +292,7 @@ CREATE TABLE IF NOT EXISTS `relay` (
 -- 表的结构 `shop`
 --
 
-CREATE TABLE IF NOT EXISTS `shop` (
+CREATE TABLE `shop` (
   `id` bigint(20) NOT NULL,
   `name` text NOT NULL,
   `price` decimal(12,2) NOT NULL,
@@ -280,7 +300,7 @@ CREATE TABLE IF NOT EXISTS `shop` (
   `auto_renew` int(11) NOT NULL,
   `auto_reset_bandwidth` int(11) NOT NULL DEFAULT '0',
   `status` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -288,7 +308,7 @@ CREATE TABLE IF NOT EXISTS `shop` (
 -- 表的结构 `speedtest`
 --
 
-CREATE TABLE IF NOT EXISTS `speedtest` (
+CREATE TABLE `speedtest` (
   `id` bigint(20) NOT NULL,
   `nodeid` int(11) NOT NULL,
   `datetime` bigint(20) NOT NULL,
@@ -309,7 +329,7 @@ CREATE TABLE IF NOT EXISTS `speedtest` (
 -- 表的结构 `ss_invite_code`
 --
 
-CREATE TABLE IF NOT EXISTS `ss_invite_code` (
+CREATE TABLE `ss_invite_code` (
   `id` int(11) NOT NULL,
   `code` varchar(128) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -323,7 +343,7 @@ CREATE TABLE IF NOT EXISTS `ss_invite_code` (
 -- 表的结构 `ss_node`
 --
 
-CREATE TABLE IF NOT EXISTS `ss_node` (
+CREATE TABLE `ss_node` (
   `id` int(11) NOT NULL,
   `name` varchar(128) NOT NULL,
   `type` int(3) NOT NULL,
@@ -345,7 +365,7 @@ CREATE TABLE IF NOT EXISTS `ss_node` (
   `node_group` int(11) NOT NULL DEFAULT '0',
   `custom_rss` int(11) NOT NULL DEFAULT '0',
   `mu_only` int(11) DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- 转存表中的数据 `ss_node`
@@ -361,7 +381,7 @@ INSERT INTO `ss_node` (`id`, `name`, `type`, `server`, `method`, `info`, `status
 -- 表的结构 `ss_node_info`
 --
 
-CREATE TABLE IF NOT EXISTS `ss_node_info` (
+CREATE TABLE `ss_node_info` (
   `id` int(11) NOT NULL,
   `node_id` int(11) NOT NULL,
   `uptime` float NOT NULL,
@@ -375,7 +395,7 @@ CREATE TABLE IF NOT EXISTS `ss_node_info` (
 -- 表的结构 `ss_node_online_log`
 --
 
-CREATE TABLE IF NOT EXISTS `ss_node_online_log` (
+CREATE TABLE `ss_node_online_log` (
   `id` int(11) NOT NULL,
   `node_id` int(11) NOT NULL,
   `online_user` int(11) NOT NULL,
@@ -388,7 +408,7 @@ CREATE TABLE IF NOT EXISTS `ss_node_online_log` (
 -- 表的结构 `ss_password_reset`
 --
 
-CREATE TABLE IF NOT EXISTS `ss_password_reset` (
+CREATE TABLE `ss_password_reset` (
   `id` int(11) NOT NULL,
   `email` varchar(32) NOT NULL,
   `token` varchar(128) NOT NULL,
@@ -402,13 +422,13 @@ CREATE TABLE IF NOT EXISTS `ss_password_reset` (
 -- 表的结构 `telegram_session`
 --
 
-CREATE TABLE IF NOT EXISTS `telegram_session` (
+CREATE TABLE `telegram_session` (
   `id` bigint(20) NOT NULL,
   `user_id` bigint(20) NOT NULL,
   `type` int(11) NOT NULL,
   `session_content` text NOT NULL,
   `datetime` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -416,7 +436,7 @@ CREATE TABLE IF NOT EXISTS `telegram_session` (
 -- 表的结构 `ticket`
 --
 
-CREATE TABLE IF NOT EXISTS `ticket` (
+CREATE TABLE `ticket` (
   `id` bigint(20) NOT NULL,
   `title` longtext NOT NULL,
   `content` longtext NOT NULL,
@@ -424,7 +444,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
   `userid` bigint(20) NOT NULL,
   `datetime` bigint(20) NOT NULL,
   `status` int(11) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -432,7 +452,7 @@ CREATE TABLE IF NOT EXISTS `ticket` (
 -- 表的结构 `unblockip`
 --
 
-CREATE TABLE IF NOT EXISTS `unblockip` (
+CREATE TABLE `unblockip` (
   `id` bigint(20) NOT NULL,
   `ip` text NOT NULL,
   `datetime` bigint(20) NOT NULL,
@@ -445,7 +465,7 @@ CREATE TABLE IF NOT EXISTS `unblockip` (
 -- 表的结构 `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `user_name` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
   `email` varchar(32) NOT NULL,
@@ -499,14 +519,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   `is_hide` int(11) NOT NULL DEFAULT '0',
   `is_multi_user` int(11) NOT NULL DEFAULT '0',
   `telegram_id` bigint(20) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- 转存表中的数据 `user`
 --
 
 INSERT INTO `user` (`id`, `user_name`, `email`, `pass`, `passwd`, `t`, `u`, `d`, `plan`, `transfer_enable`, `port`, `switch`, `enable`, `type`, `last_get_gift_time`, `last_check_in_time`, `last_rest_pass_time`, `reg_date`, `invite_num`, `money`, `ref_by`, `expire_time`, `method`, `is_email_verify`, `reg_ip`, `node_speedlimit`, `node_connector`, `is_admin`, `im_type`, `im_value`, `last_day_t`, `sendDailyMail`, `class`, `class_expire`, `expire_in`, `theme`, `ga_token`, `ga_enable`, `pac`, `remark`, `node_group`, `auto_reset_day`, `auto_reset_bandwidth`, `protocol`, `protocol_param`, `obfs`, `obfs_param`, `forbidden_ip`, `forbidden_port`, `disconnect_ip`, `is_hide`, `is_multi_user`, `telegram_id`) VALUES
-(1, 'admin', 'marisn@67cc.cn', '3d1463ff6f0de789723487ff6df76ab293b1651067ecda8efa547efcab885a30', '5vSYBM', 1535166937, 47308179242, 22743043221, 'A', 757887664128, 1025, 1, 1, 1, 0, 1535040881, 0, '2018-07-30 23:56:43', 8, '0.00', 0, 0, 'rc4-md5', 0, '127.0.0.1', '0.00', 0, 1, 4, '', 65477368557, 0, 1, '2019-08-15 23:53:28', '2028-07-27 23:56:43', 'material', 'KWEXIX3EKD4AI7L4', 0, NULL, '', 0, 0, '0.00', 'origin', '', 'plain', '', '', '', NULL, 0, 0, 448288073);
+(1, 'admin', 'marisn@67cc.cn', '3d1463ff6f0de789723487ff6df76ab293b1651067ecda8efa547efcab885a30', 'VC7wzz', 0, 0, 0, 'A', 1073741824, 1025, 1, 1, 1, 0, 0, 0, '2018-08-25 13:21:13', 10, '0.00', 0, 0, 'rc4-md5', 0, '127.0.0.1', '0.00', 0, 1, 1, '', 0, 0, 0, '1989-06-04 00:05:00', '2028-08-22 13:21:13', 'material', 'T3UK5HRSSCGXJNE2', 0, NULL, NULL, 0, 0, '0.00', 'origin', NULL, 'plain', NULL, NULL, NULL, NULL, 0, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -514,7 +534,7 @@ INSERT INTO `user` (`id`, `user_name`, `email`, `pass`, `passwd`, `t`, `u`, `d`,
 -- 表的结构 `user_token`
 --
 
-CREATE TABLE IF NOT EXISTS `user_token` (
+CREATE TABLE `user_token` (
   `id` int(11) NOT NULL,
   `token` varchar(256) NOT NULL,
   `user_id` int(11) NOT NULL,
@@ -528,7 +548,7 @@ CREATE TABLE IF NOT EXISTS `user_token` (
 -- 表的结构 `user_traffic_log`
 --
 
-CREATE TABLE IF NOT EXISTS `user_traffic_log` (
+CREATE TABLE `user_traffic_log` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `u` bigint(20) NOT NULL,
@@ -728,159 +748,190 @@ ALTER TABLE `user_traffic_log`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在导出的表使用AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `alive_ip`
+-- 使用表AUTO_INCREMENT `alive_ip`
 --
 ALTER TABLE `alive_ip`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `announcement`
+-- 使用表AUTO_INCREMENT `announcement`
 --
 ALTER TABLE `announcement`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `auto`
+-- 使用表AUTO_INCREMENT `auto`
 --
 ALTER TABLE `auto`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `blockip`
+-- 使用表AUTO_INCREMENT `blockip`
 --
 ALTER TABLE `blockip`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `bought`
+-- 使用表AUTO_INCREMENT `bought`
 --
 ALTER TABLE `bought`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `code`
+-- 使用表AUTO_INCREMENT `code`
 --
 ALTER TABLE `code`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `coupon`
+-- 使用表AUTO_INCREMENT `coupon`
 --
 ALTER TABLE `coupon`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `detect_list`
+-- 使用表AUTO_INCREMENT `detect_list`
 --
 ALTER TABLE `detect_list`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `detect_log`
+-- 使用表AUTO_INCREMENT `detect_log`
 --
 ALTER TABLE `detect_log`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `disconnect_ip`
+-- 使用表AUTO_INCREMENT `disconnect_ip`
 --
 ALTER TABLE `disconnect_ip`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `email_verify`
+-- 使用表AUTO_INCREMENT `email_verify`
 --
 ALTER TABLE `email_verify`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `link`
+-- 使用表AUTO_INCREMENT `link`
 --
 ALTER TABLE `link`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
--- AUTO_INCREMENT for table `login_ip`
+-- 使用表AUTO_INCREMENT `login_ip`
 --
 ALTER TABLE `login_ip`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `payback`
+-- 使用表AUTO_INCREMENT `payback`
 --
 ALTER TABLE `payback`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `paylist`
+-- 使用表AUTO_INCREMENT `paylist`
 --
 ALTER TABLE `paylist`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `radius_ban`
+-- 使用表AUTO_INCREMENT `radius_ban`
 --
 ALTER TABLE `radius_ban`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `relay`
+-- 使用表AUTO_INCREMENT `relay`
 --
 ALTER TABLE `relay`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `shop`
+-- 使用表AUTO_INCREMENT `shop`
 --
 ALTER TABLE `shop`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `speedtest`
+-- 使用表AUTO_INCREMENT `speedtest`
 --
 ALTER TABLE `speedtest`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `ss_invite_code`
+-- 使用表AUTO_INCREMENT `ss_invite_code`
 --
 ALTER TABLE `ss_invite_code`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `ss_node`
+-- 使用表AUTO_INCREMENT `ss_node`
 --
 ALTER TABLE `ss_node`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- AUTO_INCREMENT for table `ss_node_info`
+-- 使用表AUTO_INCREMENT `ss_node_info`
 --
 ALTER TABLE `ss_node_info`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `ss_node_online_log`
+-- 使用表AUTO_INCREMENT `ss_node_online_log`
 --
 ALTER TABLE `ss_node_online_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `ss_password_reset`
+-- 使用表AUTO_INCREMENT `ss_password_reset`
 --
 ALTER TABLE `ss_password_reset`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `telegram_session`
+-- 使用表AUTO_INCREMENT `telegram_session`
 --
 ALTER TABLE `telegram_session`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `ticket`
+-- 使用表AUTO_INCREMENT `ticket`
 --
 ALTER TABLE `ticket`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `unblockip`
+-- 使用表AUTO_INCREMENT `unblockip`
 --
 ALTER TABLE `unblockip`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `user`
+-- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
--- AUTO_INCREMENT for table `user_token`
+-- 使用表AUTO_INCREMENT `user_token`
 --
 ALTER TABLE `user_token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `user_traffic_log`
+-- 使用表AUTO_INCREMENT `user_traffic_log`
 --
 ALTER TABLE `user_traffic_log`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
